54cd14970e8f0075e0999e57f4c67ac34bf23640
kunj-champaneri <kunjchampaneri777@gmail.com>
2021-03-15 15:40:20 -0500

    Update SecureAuth.py

--- SecureAuth.py
+++ SecureAuth.py
@@ -11,6 +11,8 @@ import csv
 def secure_hashed_passwd(username, passwd):
     import hashlib, uuid
     import os
+    import sys
+    import random
 
     '''
     @TODO: Students are required to implement this function.
@@ -20,7 +22,35 @@ def secure_hashed_passwd(username, passwd):
     :return: True if given values are stored successfully in outfile var; else returns False
     '''
 
+    #H object for algorithm
+    h = hashlib.sha256()
     
+    #salt 
+    random_number = random.randint(0,16777215) #Generating Random value
+    hex_number = str(hex(random_number)) #Generating Random value
+    salt = hex_number
+    salt_as_bytes = str.encode(salt) #Converting into Bytes
+    h.update(salt_as_bytes) #Updating it 
+    #salt_hex = h.hexdigest()
+    print(salt_as_bytes) #Debug 
+    
+    #pepper
+    random_number = random.randint(0,16777215) #Generating Random value
+    hex_number = str(hex(random_number)) #Generating Random value
+    pepper = hex_number
+    pepper_as_bytes = str.encode(pepper) #Converting into Bytes
+    h.update(pepper_as_bytes) #Updating it
+    #pepper_hex = h.hexdigest()
+    print(pepper_as_bytes) #Debug 
+    
+    #salt+pepper+password
+    password_as_bytes = str.encode(passwd) #Converting into Bytes
+    h.update(password_as_bytes) #Updating it 
+    final_hex = h.hexdigest() #Converting the final value to hexadecimal 
+    print(final_hex) #Debug 
+    
+    #Question: Do I need to store it in the file?
+    #Question: Are the values which are printing out for salt and pepper are correct?
 
 
     #use salt and pepper to hash 'hpasswd' using sha-3-224 algorithm
